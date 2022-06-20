f010204eb9ef204c7c16fbb9eeeb1c7e0f66a366
Itz-Djin <Christdjin.B@gmail.com>
2022-06-19 20:46:06 -0600

    update on NaN side practice

--- JavaScript/Assignments/Nan_Practice/JS/main.js
+++ JavaScript/Assignments/Nan_Practice/JS/main.js
@@ -1,9 +1,10 @@
-function my_Function() {
-    document.getElementById("Test").innerHTML = 0 / 0;
+function my_Function() { // Created the function "my_Function"
+    document.getElementById("Test").innerHTML = 0 / 0; // Returns NaN(Not a Number)
 }
 
-document.getElementById("NaN_T").innerHTML = isNaN("Hello World!");
-document.getElementById("NaN_F").innerHTML = isNaN('555');
+document.getElementById("NaN_T").innerHTML = isNaN("Hello World!"); // returns True becuse the isNaN method asks if the value in parenthesis is NOT a number, in this case it's a string. 
+document.getElementById("NaN_F").innerHTML = isNaN('555'); // returns false because, although it is a string it is a number.
 document.write("<br>");
 document.write(2E310);
+document.write("<br>");
 document.write(-2E310);

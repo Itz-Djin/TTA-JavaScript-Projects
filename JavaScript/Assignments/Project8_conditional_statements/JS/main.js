2e083bbf2e2c5cfcf240b2277dc11df1cb74c7df
Itz-Djin <Christdjin.B@gmail.com>
2022-06-21 14:11:02 -0600

    Created IF statement assignment

--- JavaScript/Assignments/Project8_conditional_statements/JS/main.js
+++ JavaScript/Assignments/Project8_conditional_statements/JS/main.js
@@ -1,6 +1,6 @@
 // Practice IF statements. Section 2, Subsection E, step 145: IF statements
 if (1 < 2) { // stating that if the statement is true, which it is, display the method below
-    document.write("The left number is smaller than the number on the right.")
+    document.write("The left number is smaller than the number on the right." + "<br>");
 }
 function get_Date() { // the if condition below states that if it is before (less than) 6:00pm, display our string in the html element with designated ID, if this code is ran later than 6:00pm, nothing would display. 
     if (new Date().getHours() < 18) { // the method here returns the hour in the specified datte according to the local time, and hours are listed as integers between 0 and 23. 
@@ -17,3 +17,15 @@ function dateFunction() { // created dateFunction that will utilized an if state
         document.getElementById('MethodAssignment').innerHTML = x; // this variable is only displayed based on the condition of the if statetemnt is met
     }
 }
+
+/*==========================
+END OF METHOD ASSIGNMENT
+START OF IF STATEMENT ASSIGNMENT
+============================*/
+if (20 / 10 == 8 / 4) { // created if statement saying that if the numbers in are equal display the string in the document
+    document.write("THEY ARE EQUAL!");
+}
+/*==========================
+END OF IF STATEMENT ASSIGNMENT
+START OF
+============================*/

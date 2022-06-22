38d7a6fa709363f503ae0e51f8e01564192903bd
Itz-Djin <Christdjin.B@gmail.com>
2022-06-21 23:45:03 -0600

    Created for_loop() assignment

--- JavaScript/Assignments/Project10_loops_arrays/JS/main.js
+++ JavaScript/Assignments/Project10_loops_arrays/JS/main.js
@@ -16,6 +16,7 @@ function count_To_Ten() { // created function count_To_Ten
 END LOOPS PRACTICE/DEMO
 LOOPS ASSIGNMENT
 ===============*/
+
 const cars = ["Toytoa", "Mercedes", "Fiat", "Ford", "Hyundai", "Honda", "Nissan", "Mitsubishi"];
 function callLoopFunction() {
     var text = "";
@@ -26,3 +27,37 @@ function callLoopFunction() {
     document.getElementById("Looper").innerHTML = text;
     
 }
+
+/*=============
+END LOOPS ASSIGNMENT
+LENGTH PROPERTY CHALLENGE
+===============*/
+
+/*Define and learn how to use the JavaScript string length property.
+1) length property: returns the length of a string
+*/
+
+function lengthPropFunction() { // created lengthPropFunction
+    var x = "Click to display the length of this string." // created variable x with string data type
+    document.getElementById("Length_Prop").innerHTML = x.length; // displays the length of the string data type given to variable x
+}
+
+/*=============
+END LENGTH PROPERTY CHALLENGE
+FOR LOOP PRACTICE/DEMO
+===============*/
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"]; // is this an array?
+var Content = ""; // why do we keep creating a variable with a blank string?
+var Y; // declares initial variable
+function for_Loop() { // created for_Loop() function
+    for (Y = 0; Y < Instruments.length; Y++) { // Use for statement, assigns value 0 to variable Y, uses property length on variable Instruments (Length==7), saying that if Y which starts at 0 is less than that increment it by 1. This is repeated until the condition is met, which is when Y increments 7 times, or the block of code is iterated through 7 times
+        Content += Instruments[Y] + "<br>"; // what are the brackets for and yielding the Y variable to the instruments?
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+/*=============
+END FOR LOOP PRACTICE/DEMO
+
+===============*/

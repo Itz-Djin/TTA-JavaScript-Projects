dec4aeca4658a1b79293e0bb244f1ba8c63a5b44
Itz-Djin <Christdjin.B@gmail.com>
2022-06-21 22:28:15 -0600

    Added comments

--- JavaScript/Assignments/Project11_Defer_Challenge/JS/main.js
+++ JavaScript/Assignments/Project11_Defer_Challenge/JS/main.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var x = 1;
-    x++;
-    document.getElementById("p1").innerHTML = x;
+function myFunction() { // created myFunction
+    var x = 1; // created variable x with a value of 1
+    x++; // incremented variable x by 1
+    document.getElementById("p1").innerHTML = x; // outputs value of variable x to designated HTML element
 }

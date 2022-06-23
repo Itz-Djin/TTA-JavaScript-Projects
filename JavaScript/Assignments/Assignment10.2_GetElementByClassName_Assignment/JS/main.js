195094a1c619fd3b6a474ed6c3c5f8a20e21923e
Itz-Djin <Christdjin.B@gmail.com>
2022-06-22 18:18:12 -0600

    Completed GetElementByClassName() Method Assignment

---
+++ JavaScript/Assignments/Assignment10.2_GetElementByClassName_Assignment/JS/main.js
@@ -0,0 +1,4 @@
+function classNameFunction() {
+    var x = document.getElementsByClassName("Test");
+    x[0].innerHTML = "Congrats! We've made the change by specifying the index 0 to change to this string!";
+}
---
+++ JavaScript/Assignments/Practices/GetElementByClassName()Method/JS/main.js
@@ -0,0 +1,6 @@
+// GETELEMENTBYCLASSNAME() METHOD PRACTICE/DEMO
+
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}

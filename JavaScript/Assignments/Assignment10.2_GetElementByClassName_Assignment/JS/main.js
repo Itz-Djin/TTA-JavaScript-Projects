2f3dbde07a13825480567d56b2fb8b869de6b139
Itz-Djin <Christdjin.B@gmail.com>
2022-06-22 18:21:14 -0600

    Completed Canvas Element Assignment

--- JavaScript/Assignments/Assignment10.2_GetElementByClassName_Assignment/JS/main.js
+++ JavaScript/Assignments/Assignment10.2_GetElementByClassName_Assignment/JS/main.js
@@ -1,3 +1,4 @@
+// GetElementByClassName() Method Assignment
 function classNameFunction() {
     var x = document.getElementsByClassName("Test");
     x[0].innerHTML = "Congrats! We've made the change by specifying the index 0 to change to this string!";

7c1a1b88aeffc98ccc1a7ecac45ed67bd07a7006
Itz-Djin <Christdjin.B@gmail.com>
2022-06-27 15:26:13 -0600

    Update visuals/audio
    change mp3 sounds for placement, tie games, and win game. Changed the "X" and "O" images

--- JavaScript/Basic_JavaScript_Projects/Large_Projects/TicTacToe/js/TicTacToe.js
+++ JavaScript/Basic_JavaScript_Projects/Large_Projects/TicTacToe/js/TicTacToe.js
@@ -14,11 +14,11 @@ function placeXOrO(squareNumber) {
         //this condition checks who's turn it is
         if (activePlayer === 'X') {
             //if activePlayer is equal to 'X', the x.png is placed in HTMl
-            select.style.backgroundImage = 'url("images/x.png")';
+            select.style.backgroundImage = 'url("images/metalX187px.jpg")';
             //active player may onlyu be 'X' or 'O' so, if not 'X' must be 'O'
         } else {
             //if activePlayer is equal to 'O', the o.png is placed in HTML
-            select.style.backgroundImage = 'url("images/o.png")';
+            select.style.backgroundImage = 'url("images/metalO187px.jpg")';
         }
         // squareNumber and activePlayer are concatenated together and added to array
         selectedSquares.push(squareNumber + activePlayer);
@@ -34,7 +34,7 @@ function placeXOrO(squareNumber) {
             activePlayer = 'X';
         }
         //this function plays placement sound
-        audio('./media/place.mp3');
+        audio('./media/zapPlace.mp3');
         //this condition checks to see if it is the cpu turn
         if (activePlayer === 'O') {
             //this function disables clicking for computers turn
@@ -105,7 +105,7 @@ function checkWinConditions() {
     // 9 squares are selected the code executes.
     else if (selectedSquares.length >= 9) {
         // this function plays the tie game sound.dddddd
-        audio('./media/tie.mp3');
+        audio('./media/zapTie.mp3');
         // this function sets a .3 second timer before the resetGame is called.
         setTimeout(function () { resetGame(); }, 500);
     }
@@ -205,7 +205,7 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
     //this line disallows clicking while the win sound is playing
     disableClick();
     //this line plays the win sounds
-    audio('./media/winGame.mp3');
+    audio('./media/zapWin.mp3');
     //this line calls our main animation loop
     animateLineDrawing();
     //this line waits 1 second. then, clears canvas, rests game, an allows clicking again
